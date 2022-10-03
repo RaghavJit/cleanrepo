700aecc271dede792cc41bfc1a5e9808af7189ea
Anoni <amr@tota.com>
2022-10-03 15:35:25 +0530

    Added connections (finally)

--- New folder/Max Power Distt. Theorem/Function/function.js
+++ New folder/Max Power Distt. Theorem/Function/function.js
@@ -1,24 +1,62 @@
-var up = document.getElementById("up")
-var down = document.getElementById("down")
-var variacImg = document.getElementById("variacImg")
-var index = 1;
+var cont = document.getElementById("connections_buttons")
 
-up.onclick = function GoUp(){
-    index = index + 1;
-    switch(index){
-        case 5:
-            index = 1;
-        case 1:
-            variacImg.src = '..Assets/CFL_Off.png';
-            break;
-        case 2:
-            variacImg.src = '..Assets/Lamp_Off.png';
-            break;
-        case 3:
-            variacImg.src = '..Assets/Led_Off.png';
-            break;
-        case 4:
-            variacImg.src = '..Assets/tubelight_Off.png';
-            break;
-    }
+var VOLTAGE_POSITIVE = document.getElementById("p_v")
+var VOLTAGE_NEGATIVE = document.getElementById("n_v")
+var AMMETER_POSITIVE = document.getElementById("p_a")
+var AMMETER_NEGATIVE = document.getElementById("n_a")
+var MULTIMETER_POSITIVE = document.getElementById("p_m") 
+var MULTIMETER_NEGATIVE = document.getElementById("n_m") 
+var POWER_POSITIVE = document.getElementById("p_p")
+var POWER_NEGATIVE = document.getElementById("n_p")
+var POWER_ON = document.getElementById("p_on")
+var CIRCUIT_POWER_POSITIVE = document.getElementById("c_p_p") 
+var CIRCUIT_POWER_NEGATIVE = document.getElementById("c_p_n") 
+var CIRCUIT_AMMETER_POSITIVE = document.getElementById("c_a_p") 
+var CIRCUIT_AMMETER_NEGATIVE = document.getElementById("c_a_n")  
+var CIRCUIT_VOLTAGE_POSITIVE = document.getElementById("c_v_p") 
+var CIRCUIT_VOLTAGE_NEGATIVE = document.getElementById("c_v_n")   
+var MCB_SWITCH = document.getElementById("mcb_switch")
+var toggle = false
+MCB_SWITCH.onclick = function() {
+    
 }
+
+const instance = jsPlumb.getInstance({
+    container: cont
+});
+
+instance.bind("ready", function() {
+    instance.registerConnectionTypes({
+        "positive": {
+            paintStyle: { stroke: "rgb(97,106,229)", strokeWidth: 2.5 },
+            hoverPaintStyle: { stroke: "rgb(97,106,229)", strokeWidth: 3.5 }
+        },
+        "negative": {
+            paintStyle: { stroke: "rgb(229, 97, 97)", strokeWidth: 2.5 },
+            hoverPaintStyle: { stroke: "rgb(229, 97, 97)", strokeWidth: 3.5 }
+        }
+    });
+
+    instance.addEndpoint([POWER_POSITIVE, AMMETER_POSITIVE, VOLTAGE_POSITIVE, AMMETER_POSITIVE, MULTIMETER_POSITIVE, CIRCUIT_POWER_POSITIVE, CIRCUIT_AMMETER_POSITIVE, CIRCUIT_VOLTAGE_POSITIVE], { //POWER_POSITIVE, AMMETER_POSITIVEP, VOLTAGE_POSITIVE, AMMETER_POSITIVE, MULTIMETER_POSITIVE, CIRCUIT_POWER_POSITIVE, CIRCUIT_AMMETER_POSITIVE, CIRCUIT_VOLTAGE_POSITIVE
+        endpoint: "Dot",
+        anchor: ["Center"],
+        isSource: true,
+        isTarget: true,
+        paintStyle: { fill: "rgb(97,106,229)", strokeWidth: 2.5 },
+        connectionType: "positive",
+        connectionsDetachable: true,
+        maxConnections: 1
+    });
+
+    instance.addEndpoint([POWER_NEGATIVE, AMMETER_NEGATIVE, VOLTAGE_NEGATIVE, MULTIMETER_NEGATIVE, CIRCUIT_POWER_NEGATIVE, CIRCUIT_AMMETER_NEGATIVE, CIRCUIT_VOLTAGE_NEGATIVE], {
+        endpoint: "Dot",
+        anchor: ["Center"],
+        isSource: true,
+        isTarget: true,
+        paintStyle: { fill: "rgb(229, 97, 97)", strokeWidth: 2.5 },
+        connectionType: "negative",
+        connectionsDetachable: true,
+        maxConnections: 1
+    });
+
+});
